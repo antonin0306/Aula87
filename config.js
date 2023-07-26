8eeb6c5bd17e6b95a8b107d93c893c6fbcb3b533
antonin0306 <84185944+antonin0306@users.noreply.github.com>
2023-07-26 15:00:58 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC08Re9-PA8qv3qJjKOENXf8aVz99Thl4Y",
+  authDomain: "appnarracaodehistorias-9ef89.firebaseapp.com",
+  databaseURL: "https://appnarracaodehistorias-9ef89-default-rtdb.firebaseio.com",
+  projectId: "appnarracaodehistorias-9ef89",
+  storageBucket: "appnarracaodehistorias-9ef89.appspot.com",
+  messagingSenderId: "391875932138",
+  appId: "1:391875932138:web:205c850c50d7eb88620c93"
+};
+
